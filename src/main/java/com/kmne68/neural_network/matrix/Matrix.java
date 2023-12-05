1b193d7a5e7e5e897167ae4da093f533dbb9760d
Keith Emery <kemery@pnmg.com>
2023-12-05 14:20:57 -0600

    Add matrix class

---
+++ src/main/java/com/kmne68/neural_network/matrix/Matrix.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.kmne68.neural_network.matrix;
+
+/**
+ *
+ * @author kemery
+ */
+public class Matrix {
+  
+  public interface Producer {
+    double produce(int index);
+  }
+  
+  
+  private double[] a;
+  
+  public Matrix(int rows, int cols) {
+    a = new double[rows * cols];
+  }
+  
+  public Matrix(int rows, int cols, Producer producer) {
+    a = new double[rows * cols];
+    
+    for(int i = 0; i < a.length; i++) {
+      a[i] = producer.produce(i);
+    }
+  }
+}
---
+++ src/test/java/com/kmne68/neural_network/matrix/MatrixTest.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit3TestClass.java to edit this template
+ */
+package com.kmne68.neural_network.matrix;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author kemery
+ */
+public class MatrixTest extends TestCase {
+  
+  public MatrixTest(String testName) {
+    super(testName);
+  }
+  
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+  }
+  
+  @Override
+  protected void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  
+  public void constructionTest() {
+    System.out.println("Hello");
+  }
+  
+}
