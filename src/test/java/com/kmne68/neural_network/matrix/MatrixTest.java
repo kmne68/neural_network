2ba38069df326b5961d067352348fb4dabe4f48f
Keith Emery <kemery@pnmg.com>
2023-12-27 14:02:49 -0600

    Finish multiply function and test; add speed test

--- src/test/java/com/kmne68/neural_network/matrix/MatrixTest.java
+++ src/test/java/com/kmne68/neural_network/matrix/MatrixTest.java
@@ -81,9 +81,30 @@ public class MatrixTest extends TestCase {
     Matrix m1 = new Matrix(2, 3, i -> i);
     Matrix m2 = new Matrix(3, 2, i -> i);
     
+    double[] expectedValues = { 10, 13, 28, 40 };
+    Matrix expected = new Matrix(2, 2, i -> expectedValues[i]);
+    
+    System.out.println(m1);
+    System.out.println(m2);
     Matrix result = m1.multiply(m2);
     
-    System.out.println(result);
+    assertTrue(expected.equals(result));
+  }
+  
+  
+  public void testMultiplySpeed() {
+    
+    int rows = 500;
+    int cols = 500;
+    int mid = 50;
+    Matrix m1 = new Matrix(rows, mid, i -> i);
+    Matrix m2 = new Matrix(mid, cols, i -> i);
+    
+    var start = System.currentTimeMillis();
+    m1.multiply(m2);
+    var end = System.currentTimeMillis();
+    
+    System.out.printf("Matrix multiplication time taken: %dms\n", end - start);
   }
   
   
