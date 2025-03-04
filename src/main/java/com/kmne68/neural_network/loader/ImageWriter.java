08bc13dc72b619dabc82366fb856cd891bd85faf
Keith Emery <kemery@pnmg.com>
2025-03-04 14:03:23 -0600

    Add label loading and begin image writing

--- src/main/java/com/kmne68/neural_network/loader/ImageWriter.java
+++ src/main/java/com/kmne68/neural_network/loader/ImageWriter.java
@@ -2,11 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.kmne68;
+package com.kmne68.neural_network.loader;
 
-import com.kmne68.neural_network.loader.BatchData;
-import com.kmne68.neural_network.loader.Loader;
-import com.kmne68.neural_network.loader.MetaData;
 import com.kmne68.neural_network.loader.image.ImageLoader;
 import java.io.File;
 
@@ -14,43 +11,44 @@ import java.io.File;
  *
  * @author kemery
  */
-public class App {
-  
+public class ImageWriter {
+
   public static void main(String[] args) {
-    
-    System.out.println("ARGS: " + args[0]);
-    
-    if(args.length == 0) {
+
+    if (args.length == 0) {
       System.out.println("Usage: [app] < MNIST DATA DIRECTORY");
       return;
     }
-    
+
     String directory = args[0];
-    
-    if(!new File(directory).isDirectory()) {
+
+    if (!new File(directory).isDirectory()) {
       System.out.println("'" + directory + "' is not a directory");
     }
-    
-    
+
+    new ImageWriter().run(directory);
+
+  }
+
+  public void run(String directory) {
     final String trainingImages = String.format("%s%s%s", directory, File.separator, "train-images-idx3-ubyte");
     final String trainingLabels = String.format("%s%s%s", directory, File.separator, "train-labels-idx1-ubyte");
     final String testImages = String.format("%s%s%s", directory, File.separator, "t10k-images-idx3-ubyte");
-    final String testLabels = String.format("%s%s%s", directory, File.separator, "t10k-labels-idx1-ubyte"); 
+    final String testLabels = String.format("%s%s%s", directory, File.separator, "t10k-labels-idx1-ubyte");
 
     Loader trainingLoader = new ImageLoader(trainingImages, trainingLabels, 32);
     Loader testLoader = new ImageLoader(testImages, testLabels, 32);
-    
+
     trainingLoader.open();
     // testLoader.open();
-    
+
     MetaData metaData = testLoader.open();
-    
-    for(int i = 0; i < metaData.getNumberOfBatches(); i++) {
-      BatchData batchData = testLoader. readBatch();
+
+    for (int i = 0; i < metaData.getNumberOfBatches(); i++) {
+      BatchData batchData = testLoader.readBatch();
     }
-    
+
     trainingLoader.close();
     testLoader.close();
   }
-  
 }
--- src/main/java/com/kmne68/neural_network/loader/image/ImageLoader.java
+++ src/main/java/com/kmne68/neural_network/loader/image/ImageLoader.java
@@ -27,7 +27,6 @@ public class ImageLoader implements Loader {
   private ImageMetaData metaData;
 
   private Lock readLock = new ReentrantLock();
-  
 
   public ImageLoader(String imageFileName, String labelFileName, int batchSize) {
     this.imageFileName = imageFileName;
@@ -35,7 +34,6 @@ public class ImageLoader implements Loader {
     this.batchSize = batchSize;
   }
 
-  
   @Override
   public ImageMetaData open() {
 
@@ -57,115 +55,131 @@ public class ImageLoader implements Loader {
 
   @Override
   public void close() {
-    
+
     metaData = null;
 
     try {
       dsImages.close();
-    }
-    catch(Exception e) {
+    } catch (Exception e) {
       throw new LoaderException("Cannot close " + imageFileName, e);
     }
 
     try {
       dsLabels.close();
-    }
-    catch(Exception e) {
+    } catch (Exception e) {
       throw new LoaderException("Cannot close " + labelFileName, e);
     }
-    
+
   }
 
   @Override
   public ImageMetaData getMetaData() {
     return metaData;
   }
-  
 
   @Override
   public BatchData readBatch() {
-    
+
     readLock.lock();
-    
+
     try {
       ImageBatchData batchData = new ImageBatchData();
-      
+
       int inputItemsRead = readInputBatch(batchData);
       int expectedItemsRead = readExpectedBatch(batchData);
-      
-      if(inputItemsRead != expectedItemsRead) {
+
+      if (inputItemsRead != expectedItemsRead) {
         throw new LoaderException("Mismatch between images read and labels read.");
       }
-      
+
       metaData.setItemsRead(inputItemsRead);
       return batchData;
-    }
-    finally {
+    } finally {
       readLock.unlock();
     }
-    
+
   }
-  
-  
+
   private ImageMetaData readMetaData() {
-    
+
     metaData = new ImageMetaData();
-    
+
     int numberOfItems = 0;
-    
+
     try {
       int magicLabelNumber = dsLabels.readInt();
       if (magicLabelNumber != 2049) {
-         throw new LoaderException("Label file " + labelFileName + " has wrong file format.");
+        throw new LoaderException("Label file " + labelFileName + " has wrong file format.");
       }
-      
+
       numberOfItems = dsLabels.readInt();
-      
+
       metaData.setNumberOfItems(numberOfItems);
       System.out.println("Number of labels: " + numberOfItems);
-    }
-    catch(IOException e) {
+    } catch (IOException e) {
       throw new LoaderException("Unable to load " + labelFileName, e);
     }
-    
-    
+
     try {
       int magicImageNumber = dsImages.readInt();
       if (magicImageNumber != 2051) {
-         throw new LoaderException("Image file " + imageFileName + " has wrong file format.");
+        throw new LoaderException("Image file " + imageFileName + " has wrong file format.");
       }
-      
-      if(dsImages.readInt() != numberOfItems) {
+
+      if (dsImages.readInt() != numberOfItems) {
         throw new LoaderException("Image file " + imageFileName + " has a different number of items compared to labelFileName.");
       }
       System.out.println("Number of Images: " + numberOfItems);
       int height = dsImages.readInt();
       int width = dsImages.readInt();
-      
+
       metaData.setHeight(height);
       metaData.setWidth(width);
       metaData.setInputSize(width * height);
       System.out.println("Height: " + height + ", Width: " + width);
-    }
-    catch(IOException e) {
+    } catch (IOException e) {
       throw new LoaderException("Unable to read " + imageFileName, e);
     }
-    
+
     metaData.setExpectedSize(10);
     metaData.setNumberOfBatches((int) Math.ceil((double) numberOfItems) / batchSize);
-    
+
     return metaData;
   }
 
-
   private int readExpectedBatch(ImageBatchData batchData) {
-    throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    try {
+      var totalItemsRead = metaData.getTotalItemsRead();
+      var numberOfItems = metaData.getNumberOfItems();
+      var numberToRead = Math.min(numberOfItems - totalItemsRead, batchSize);
+
+      var labelData = new byte[numberToRead];
+      var expectedSize = metaData.getExpectedSize();
+      
+      var numberRead = dsLabels.read(labelData, 0, numberToRead);
+      if(numberRead != numberToRead) {
+        throw new LoaderException("Couldn't read sufficient bytes from label data");
+      }
+      
+      double[] data = new double[numberToRead * expectedSize];
+      
+      for(int i = 0; i < numberToRead; i++) {
+        
+        byte label = labelData[i];
+        
+        data[i * expectedSize + label] = 1; 
+      }
+      
+      batchData.setExpectedBatch(data);
+
+      return numberToRead;
+    } catch (IOException e) {
+      throw new LoaderException("An Error occurred reading image data.", e);
+    }
   }
-  
-  
-  
+
   private int readInputBatch(ImageBatchData batchData) {
-    
+
     try {
       var totalItemsRead = metaData.getTotalItemsRead();
       var numberOfItems = metaData.getNumberOfItems();
@@ -176,26 +190,24 @@ public class ImageLoader implements Loader {
 
       byte[] imageData = new byte[numberOfBytesToRead];
       var numberRead = dsImages.read(imageData, 0, numberOfBytesToRead); // This could be done in a loop if the ready fails to read properly
-      
-      if(numberRead != numberOfBytesToRead) {
+
+      if (numberRead != numberOfBytesToRead) {
         throw new LoaderException("Could not read sufficient bytes from image data.");
       }
       double[] data = new double[numberOfBytesToRead];
-      
-      for(int i = 0; i < numberOfBytesToRead; i++) {
+
+      for (int i = 0; i < numberOfBytesToRead; i++) {
         data[i] = (imageData[i] & 0xFF) / 255.0;
-        
+
         System.out.println("Image data: " + data[i]);
       }
       batchData.setInputBatch(data);
-      
+
       return numberToRead;
-    }
-    catch(IOException e)
-    {
+    } catch (IOException e) {
       throw new LoaderException("An Error occurred reading image data.", e);
     }
-    
+
   }
 
 }
